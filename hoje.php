f630b406ea89ef09cffbadbea17b9c97dc8c4b08
ovictorjo <ovictorjocorvinal@gmail.com>
2021-02-05 16:40:36 -0400

    Programa teste em PHP

---
+++ hoje.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>Dia <?php echo date('d'); ?></title>
+    </head>
+
+    <body>
+        <h1>Estamos em <?php echo date('Y');?></h1>
+        <p>
+            Agora são <?php echo date('H');?> horas e <?php 
+            echo date('m');?> minutos.
+        </p>
+    </body>
+</html>
